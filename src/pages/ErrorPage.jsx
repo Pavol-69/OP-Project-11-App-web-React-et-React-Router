51475a54bf835bca1d9c1a97e42b17bd862171a0
Pavol-69 <paul.valy@gmail.com>
2024-12-31 14:20:30 +0100

    ajout page 404

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -2,11 +2,25 @@
 import Header from "../components/header/Header";
 import Footer from "../components/Footer";
 
+// Style
+import "../style/generalCSS.scss";
+import "../style/pages/ErrorPage.scss";
+
+// Autre
+import { Link } from "react-router-dom";
+
 function ErrorPage() {
   return (
     <>
-      <div className="main-ctn">
+      <div className="main-ctn elm-ct ver">
         <Header />
+        <span className="error error-nb">404</span>
+        <span className="error error-msg">
+          {"Oups ! La page que vous demandez n'existe pas."}
+        </span>
+        <Link className="hp-link" to="/">
+          {"Retourner sur la page d'accueil"}
+        </Link>
       </div>
       <Footer />
     </>
